0fe31ace5ea0e56fe20ad20156bac1f0e040c0f4
ruwaidah <Ruwidah.riyadh@gmail.com>
2019-10-15 17:50:52 -0500

    Movies List with save button work React Router II

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,10 +1,67 @@
-import React from 'react';
-import Movie from './Movie'
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
+
+
+
+const Movie = (props) => {
+  const [movie, setMovie] = useState();
+ 
+  useEffect(() => {
+    const id = props.movie.match.params.id;
+       axios
+        .get(`http://localhost:5000/api/movies/${id}`)
+        .then(response => {
+          setMovie(response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+
+  },[]);
+  
+  // Uncomment this only when you have moved on to the stretch goals
+   const saveMovie = () => {
+    const addToSavedList = props.movie.addToSavedList;
+    addToSavedList(movie)
+  }
+
+  if (!movie) {
+    return <div>Loading movie information...</div>;
+  }
+
+  const { title, director, metascore, stars } = movie;
+  return (
+    <div className="save-wrapper">
+      <div className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </div>
+      <div className="save-button" onClick = {
+        () => saveMovie()
+      }>Save</div>
+    </div>
+  );
+}
+
+
+
+
 
 const MovieCard = props => {
   return (
-    <Movie  id = {props.match.params.id}/>
+    <Movie  movie = {props} />
     )
 };
-
 export default MovieCard;
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,28 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 import { Link } from 'react-router-dom'
 
 
 const MovieList = props => {
-  const [movies, setMovies] = useState([])
-  useEffect(() => {
-    const getMovies = () => {
-      axios
-        .get('http://localhost:5000/api/movies')
-        .then(response => {
-          setMovies(response.data);
-        })
-        .catch(error => {
-          console.error('Server Error', error);
-        });
-    }
-    
-    getMovies();
-  }, []);
-  
   return (
     <div className="movie-list">
-      {movies.map(movie => (
+      {props.movies.map(movie => (
         <Link to = {`/Movies/${movie.id}`}>
         <MovieDetails key={movie.id} movie={movie} />
         </Link>
@@ -33,7 +16,7 @@ const MovieList = props => {
 }
 
 function MovieDetails({ movie }) {
-  console.log(movie)
+  // console.log(movie)
   const { title, director, metascore, stars } = movie;
   return (
     <div className="movie-card">
